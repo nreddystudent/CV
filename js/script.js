6628129af01b4209126f2c753db13e6dce5b1eec
nreddystudent <50164676+nreddystudent@users.noreply.github.com>
2019-09-07 12:38:20 +0200

    added typing

--- js/script.js
+++ js/script.js
@@ -5,4 +5,11 @@ $(document).ready(function(){
         play: 5000,
         pagination: false
     });
+    var typed = new Typed(".typed", {
+        strings: ["Sofware Engineer", "Web Developer", "WTC_Student"], 
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000, 
+        showCursor: false, 
+    });
 });
