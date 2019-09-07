11275ed94ab04ef554f4f119a3ab0abea17c3944
nreddystudent <50164676+nreddystudent@users.noreply.github.com>
2019-09-07 15:21:28 +0200

    looki ng better

--- js/script.js
+++ js/script.js
@@ -13,3 +13,34 @@ $(document).ready(function(){
         showCursor: false, 
     });
 });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 4,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        938:{
+            items:4
+        }
+    }
+});
+
+$('.chart').easyPieChart({
+    easing: 'easeInOut',
+    barColor: '#2980b9',
+    trackColor: '#000',
+    scaleColor: 'false',
+    lineWidth: 4,
+    size: 152,
+    onStep: function(from, to, percent){
+        $(this.el).find('.percent').text(Math.round(percent));
+    }
+});
