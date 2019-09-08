3c3e0cede89bfefa7c5feb775853b453b2a460df
nreddystudent <50164676+nreddystudent@users.noreply.github.com>
2019-09-08 23:02:59 +0200

    centered but filters not working

--- js/script.js
+++ js/script.js
@@ -12,71 +12,76 @@ $(document).ready(function(){
         startDelay: 1000, 
         showCursor: false, 
     });
-});
 
-$('.owl-carousel').owlCarousel({
-    loop:false,
-    items: 4,
-    responsive:{
-        0:{
-            items:1
-        },
-        480:{
-            items:2
-        },
-        768:{
-            items:3
-        },
-        938:{
-            items:4
-        }
-    }
-});
-
-
-var skillsTopOffset = $(".skillsSection").offset().top;
-$(window).scroll(function()
-{
-    if(window.pageYOffset > skillsTopOffset - $(window).height() + 200)
-    {
-        $('.chart').easyPieChart({
-            easing: 'easeInOut',
-            barColor: '#2980b9',
-            trackColor: '#000',
-            scaleColor: 'false',
-            lineWidth: 4,
-            size: 152,
-            onStep: function(from, to, percent){
-                $(this.el).find('.percent').text(Math.round(percent));
+    $('.owl-carousel').owlCarousel({
+        loop:false,
+        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
             }
-        });
-    }
-});
+        }
+    });
 
-$("[data-fancybox]").fancy();
 
-$(".items").isotope({
-    filter: '*',
-    animationOptions: {
-        duration: 1500,
-        easing: 'linear',
-        queue: 'false'
-    }
-});
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    $(window).scroll(function()
+    {
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200)
+        {
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#2980b9',
+                trackColor: '#000',
+                scaleColor: 'false',
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent){
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
+    });
 
- $("#filters a").click(function(){
-    $("#filters .current").removeClass("current");
-    $(this).addClass("current");
-
-    var selector = $(this).attr("data-filter");
+    $("[data-fancybox]").fancy();
 
     $(".items").isotope({
-        filter: selector,
+        filter: '*',
         animationOptions: {
             duration: 1500,
             easing: 'linear',
-            queue: false
+            queue: 'false'
         }
     });
-    return false;
+
+    $("#filters a").click(function() {
+
+        $("#filters .current").removeClass("current");
+
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: 'false'
+            }
+        });
+
+        return false;
+    });
+
 });
+
