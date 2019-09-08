7582d1b4eb2cf0d506ed6cefa88680e274e06c99
nreddystudent <50164676+nreddystudent@users.noreply.github.com>
2019-09-08 22:31:31 +0200

    added things

--- js/script.js
+++ js/script.js
@@ -52,3 +52,31 @@ $(window).scroll(function()
         });
     }
 });
+
+$("[data-fancybox]").fancy();
+
+$(".items").isotope({
+    filter: '*',
+    animationOptions: {
+        duration: 1500,
+        easing: 'linear',
+        queue: 'false'
+    }
+});
+
+ $("#filters a").click(function(){
+    $("#filters .current").removeClass("current");
+    $(this).addClass("current");
+
+    var selector = $(this).attr("data-filter");
+
+    $(".items").isotope({
+        filter: selector,
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
+    return false;
+});
