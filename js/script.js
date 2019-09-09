7ca1199aacfe812a2bf7e1839564922a7157ea7c
nreddystudent <50164676+nreddystudent@users.noreply.github.com>
2019-09-09 17:48:13 +0200

    added loading

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+
+$(window).on("load", function(){
+    $(".loader .inner").fadeOut(500, function(){
+        $(".loader").fadeOut(750);
+    });
+})
+
 $(document).ready(function(){
 
     $('#slides').superslides({
